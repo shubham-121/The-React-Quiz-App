4df6cf24b31ae581f2a8a543898e51b1b2324f17
Shubham Bhatt <81768689+shubham-121@users.noreply.github.com>
2024-09-04 15:58:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -107,7 +107,7 @@ export default function App() {
   );
 
   useEffect(function () {
-    fetch("http://localhost:9000/questions")
+    fetch("https://my-json-server.typicode.com/shubham-121/The-React-Quiz-App/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data }))
       .catch((err) => dispatch({ type: "dataFailed" }));
